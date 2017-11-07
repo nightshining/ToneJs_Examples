b84e06c1809e0de6e3ab03e28ff600b374ccd752
Mike Romeo <michaelpromeo@gmail.com>
2017-11-07 16:56:57 -0500

    @nightshining array of osc working

--- examples/osc_array/oscarray.js
+++ examples/osc_array/oscarray.js
@@ -1,46 +1,67 @@
-
+//Examples Oscillator Array//
 var counter = 0;
-var counterMax = 16;
-var beat = ["4n","8n","16n","32n"]
-var oscillators = [];
+var counterMax = 2;
+var beat = ["4n","8n","16n","32n"];
 var oscAmt = 8;
-var baseFreq = 210;
-var value;
+var oscillators = [];
+var baseFreq = 50;
 
   //Tone js Instruments//
+
+  //Single Noise Oscillator//
+  var noise = new Tone.Noise().toMaster();
+  noise.start();
+  noise.volume.value = -20;
+  noise.send("reverb");
+  noise.send("delay");
+
+  //Array Sine Oscillators//
   for (var i = 0; i < oscAmt; i++){
         oscillators.push(new Tone.Oscillator({
         "frequency" : baseFreq * i,
-        "type" :  "square",
+        "type" :  "triangle",
+        "volume" : -10,
         "detune" : Math.random() * 30 - 15
       }).toMaster().start());
-      oscillators[i].send("reverb", -10);
 
+      oscillators[i].send("reverb", -10);
+      oscillators[i].send("delay", -5);
     }
 
   //Tone js Effects//
   var reverb = new Tone.Freeverb(0.95, 4000).receive("reverb").toMaster();
-  var delay = new Tone.FeedbackDelay(beat[0],15).receive("delay").toMaster();
+  var delay = new Tone.FeedbackDelay(beat[1],0.75).receive("delay").toMaster();
 
   //Tone js Transport//
-  Tone.Transport.bpm.value = 130;
-  Tone.Transport.scheduleRepeat(function(time){
-     counter++;
-     counter = counter % counterMax;
-     //console.log(counter)
-   }, beat[0]);
-  Tone.Transport.start();
   Tone.Master.volume.value = -40;
+  Tone.Master.bpm = 160;
 
-//Logic//
+  //Logic//
 
+  function loopCallback(time){
+      counter++;
+      counter = counter % counterMax;
+      if(counter == counterMax - 1) {
+          var r = Math.random()*10;
+          var index = Math.abs(Math.round(Math.random()*oscAmt - 1));
+          var vIndex = -Math.round(Math.random()*30);
+          oscillators[index].frequency.rampTo(baseFreq * r, 5)
+          oscillators[index].volume.rampTo(vIndex, 0.4)
+          noise.volume.rampTo(vIndex, 1);
+          console.log("Ramp osc at index: " + index + " volume " + vIndex );
+        }
+      }
 
+    var loop = new Tone.Loop(loopCallback, 2);
+    loop.start(0);
+    Tone.Transport.start(0);
 
-window.addEventListener('mousemove', function (e) {
-    //console.log("Mouse X: " + e.x + " Mouse Y: " + e.y);
-    value = e.x * 0.001;
-    console.log(value);
-    oscillators.forEach(function(osc, i){
-          osc.frequency.rampTo(baseFreq * i * value, 0.2);
-     });
-})
+//note: to change all frequencies//
+// window.addEventListener('mousemove', function (e) {
+//     //console.log("Mouse X: " + e.x + " Mouse Y: " + e.y);
+//     value = e.x * 0.001;
+//     console.log(value);
+//      oscillators.forEach(function(osc, i){
+//            osc.frequency.rampTo(baseFreq * i * value, 0.2);
+//       });
+// })
